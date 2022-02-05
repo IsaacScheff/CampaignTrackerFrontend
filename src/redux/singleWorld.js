afd90ec6ed33acb8d9b17bfcfb0cf1884e51c6f8
Isaac Scheff <isaac@Isaacs-MacBook-Pro.local>
2022-02-04 23:29:36 -0500

    Feature: updating individual worlds/campaigns

--- src/redux/singleWorld.js
+++ src/redux/singleWorld.js
@@ -1,9 +1,15 @@
 import Axios from 'axios';
 
 const SET_SINGLE_WORLD = 'SET_SINGLE_WORLD';
+const UPDATE_WORLD = 'UPDATE_WORLD';
 
 export const setSingleWorld = (world) => ({
     type: SET_SINGLE_WORLD,
+    world
+});
+
+export const _updateWorld = (world) => ({
+    type: UPDATE_WORLD,
     world
 });
 
@@ -18,11 +24,22 @@ export const fetchSingleWorld = (id) => {
     }
 }
 
+export const updateWorld = (world) => async(dispatch) => {
+    try {
+      const {data: updated} = await Axios.put(`http://localhost:1337/worlds/${world.id}`, world);
+      dispatch(_updateWorld(updated));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
 const initialState = {};
 
 export default function singleWorldReducer(state = initialState, action) {
     switch(action.type){
         case SET_SINGLE_WORLD:
+            return action.world;
+        case UPDATE_WORLD:
             return action.world;
         default:
             return state;
