d085b2b63e3f498f094903a552420d061ae91ab8
Isaac Scheff <isaac@Isaacs-MacBook-Pro.local>
2022-02-07 07:49:37 -0500

    Put github and linked in logos on about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,15 +1,21 @@
 import React from "react";
-
+import linkedIn from '../assets/LI-Logo.png';
+import github from '../assets/GitHub_Logo.png'
 export function AboutPage () {
     
     return (
         <div className='about-page'>
             Created with the React javascript library by Isaac Scheff
+            <p>Find me on</p>
             <p>
-                <a href="https://www.linkedin.com/in/isaac-scheff/">LinkedIn</a>  {/* replace with LinkedIn logo*/}
+                <a href="https://www.linkedin.com/in/isaac-scheff/"> 
+                    <img className="logo-link" src={linkedIn} alt="Linked In Logo"/>
+                </a> 
             </p>
             <p>
-                <a href="https://github.com/IsaacScheff/">GitHub</a>
+                <a href="https://github.com/IsaacScheff/">
+                    <img className="logo-link" src={github} alt="GitHub Logo"/>
+                </a>
             </p>
         </div>
     )
