6feab2b9a53d66074313e6d5b48e49a6085b3c54
Isaac Scheff <isaac@Isaacs-MacBook-Pro.local>
2022-02-07 04:55:22 -0500

    Basic information put on homepage and About page.

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -4,14 +4,13 @@ export function AboutPage () {
     
     return (
         <div className='about-page'>
+            Created with the React javascript library by Isaac Scheff
             <p>
                 <a href="https://www.linkedin.com/in/isaac-scheff/">LinkedIn</a>  {/* replace with LinkedIn logo*/}
             </p>
             <p>
                 <a href="https://github.com/IsaacScheff/">GitHub</a>
             </p>
-            Created by Isaac Scheff
-            other info...
         </div>
     )
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,10 +4,18 @@ export function HomePage () {
     
     return (
         <div className='home-page'>
-            Home page
-            <p>
-                Info/instructions here
-            </p>
+            <h className='campaign-title'>
+                Welcome to Campaign Tracker! 
+            </h>
+            <div className='home-page-text'>
+                <ul>
+                    <li>Campaign Tracker is a structured forum for group note taking for Tabletop Role-Playing games</li> 
+                    <li>Your group first creates a Campaign, with an image and a description</li>
+                    <li>You can then make individual Posts within that Campaign, tagged with a topic such as NPC or location or anything else that suits you</li> 
+                    <li>Posts can also have pictures associated with them. Within each Post you can further the discusson with Comments</li> 
+                    <li>You may edit your Campaign and Post information, and delete any unwanted Campaigns, Posts, and Comments</li>
+                </ul>  
+            </div>
         </div>
     )
 }
