30b74a505fa49047e9b197aa02c7ae619b5d502a
Isaac Scheff <isaac1996@comcast.net>
2022-04-09 13:34:22 -0400

    Random Campaign button added to campaign list page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button, TextField, Paper } from "@mui/material";
  
 
 
 const Navbar = () => {
   return (
-        <nav className="navbar">
-            .
-            <Link to ='/'>Home</Link>
-            <Link to ='/worlds'>Campaign List</Link>
-            <Link to ='/createnewcampaign'>Create New Campaign</Link>
-            <Link to ='/about'>About</Link>
-            .
-        </nav>
+        <Paper sx={{
+            height: "40px"
+        }}>
+            <nav className="navbar">
+                .
+                <Link to ='/'>Home</Link>
+                <Link to ='/worlds'>Campaign List</Link>
+                <Link to ='/createnewcampaign'>Create New Campaign</Link>
+                <Link to ='/about'>About</Link>
+                .
+            </nav>
+        </Paper>
     )
 };
 
--- src/dev.js
+++ src/dev.js
@@ -1,4 +1,4 @@
-//const api = 'http://localhost:1337'
-const api = 'https://api.campaigntracker.org'
+const api = 'http://localhost:1337'
+//const api = 'https://api.campaigntracker.org'
 
 export default api;
